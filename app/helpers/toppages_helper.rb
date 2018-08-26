9399486f5f67afcd3aef3aeae47abf2d26b3245d
EikiKanaoka <kakakaeee@gmail.com>
2018-08-26 17:16:55 +0900

    - Add pokemon image public/pokemon_images - Imaged the style of the toppages/index.html.erb

--- app/helpers/toppages_helper.rb
+++ app/helpers/toppages_helper.rb
@@ -1,2 +1,14 @@
 module ToppagesHelper
+
+  # トップページのドット絵のランダムに使用
+  def random_spr
+   @rand = rand(1..721)
+   if @rand >= 1 && @rand <= 9
+     @rand_spr = "00" + @rand.to_s
+   elsif @rand >= 10 && @rand <= 99
+     @rand_spr = "0" + @rand.to_s
+   else
+     @rand_spr = @rand.to_s
+   end
+  end
 end
