d962325fdbf3afb1868b1c77406c5e24e10468e7
EikiKanaoka <kakakaeee@gmail.com>
2018-08-28 19:21:55 +0900

    Generate Post controller  - resources posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,27 @@
+class PostsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def params_post
+  end
+end
