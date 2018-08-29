2836eb43065fb9b13493c19a9c666584191047a9
EikiKanaoka <kakakaeee@gmail.com>
2018-08-29 19:58:19 +0900

    TopPages#index ランキングパーティの基礎を作成 - PostModelに保存された世代別のCreatd_at順に３パーティ分を表示 - Rainkingを作成したらRanking順に変更する - seed.rbでサンプルパーティを作成（各世代５パーティずつ） - styleは特に変わりなし - viewにeach文条件文を使いまわしているので若干嫌だ

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,6 +1,12 @@
 class ToppagesController < ApplicationController
   def index
-    @posts = Post.all
+    @red_green = Post.where(game: "red_green").limit(5)
+    @gold_silver = Post.where(game: "gold_silver").limit(5)
+    @ruby_sapphire = Post.where(game: "ruby_sapphire").limit(5)
+    @diamond_pearl = Post.where(game: "diamond_pearl").limit(5)
+    @black_white = Post.where(game: "black_white").limit(5)
+    @x_y = Post.where(game: "x_y").limit(5)
+    @sun_moon = Post.where(game: "sun_moon").limit(5)
   end
 
   def show
